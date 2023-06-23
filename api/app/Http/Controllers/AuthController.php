63363f88f7c2790465fa2541b13e2fd49b3d12af
Victor Akinyokun <vicgbadebo@gmail.com>
2023-06-23 15:28:30 +0100

    authentication with sanctum

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use \Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Response;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return Response::json(['message' => 'login successful']);
+        }
+
+        return Response::json(['message' => 'wrong login credentials'], 403);
+    }
+
+    public function register(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+            'username' => ['required'],
+        ]);
+
+        if(User::where('email', $request->email)->exists()) {
+            return Response::json(['message' => 'email already exists'], 400);
+        }
+
+        $user = User::create([
+            'name' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'email_verified_at' => now(),
+        ]);
+
+        if($user) return Response::json(['message' => 'Registration successfull. You can now login']);
+    }
 }
